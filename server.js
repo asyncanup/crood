898b3bbddb536c07769ee236b2edfa011e5f0eb7
Anup Bishnoi <pixelsallover@gmail.com>
2014-06-26 22:44:30 +0530

    now listens on 0.0.0.0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require("apper")();
 
 if (app.init()) {
-	app.start(8000, function () {
+	app.start(8000, "0.0.0.0", function () {
 		console.log("Serving on " + this.address().port);
 	});
 }
